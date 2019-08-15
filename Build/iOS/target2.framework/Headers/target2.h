7dffb30ddbf3e57104d838a1f9b568117d4a8335
zoeyliang@tencent.com <zoeyliang@tencent.com>
2019-08-15 21:00:08 +0800

    注释语句

---
+++ Build/iOS/target2.framework/Headers/target2.h
@@ -0,0 +1,19 @@
+//
+//  target2.h
+//  target2
+//
+//  Created by admin on 2019/8/15.
+//  Copyright © 2019年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for target2.
+FOUNDATION_EXPORT double target2VersionNumber;
+
+//! Project version string for target2.
+FOUNDATION_EXPORT const unsigned char target2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <target2/PublicHeader.h>
+
+
