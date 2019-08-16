38d8a1e020ef304c0258b352f76b9004819eacbd
zoeyliang@tencent.com <zoeyliang@tencent.com>
2019-08-16 13:24:04 +0800

    更新头文件

--- target2/target2.h
+++ target2/target2.h
@@ -17,3 +17,6 @@ FOUNDATION_EXPORT const unsigned char target2VersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <target2/PublicHeader.h>
 
 
+#import "XGPush.h"
+#import "MTA.h"
+#import "MTAConfig.h"
